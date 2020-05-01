8371dbc4c0275d7eb9cbba10bc8713bfd0c06773
pricejoshua <joshua.j.price@my.wheaton.edu>
2020-04-30 20:02:31 -0700

    added create_user

---
+++ agenda-buddy/create_user.php
@@ -0,0 +1,48 @@
+<?php
+ 
+/*
+ * Following code will create a new product row
+ * All product details are read from HTTP Post Request
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// check for required fields
+if (isset($_POST['email'])){
+ 
+    $email = $_POST['email'];
+    
+    require_once __DIR__ . '/db_config.php';
+ 
+    // connecting to db
+    $con = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE) or die(mysqli_error());
+
+    // mysql inserting a new row
+    $result = mysqli_query($con, "INSERT INTO user(email) VALUES('$email')");
+ 
+    // check if row inserted or not
+    if ($result) {
+        // successfully inserted into database
+        $response["success"] = 1;
+        $response["message"] = "User successfully created.";
+ 
+        // echoing JSON response
+        echo json_encode($response);
+    } else {
+        // failed to insert row
+        $response["success"] = 0;
+        $response["message"] = "Oops! An error occurred.";
+        $response["error"] = mysqli_errno($con);
+ 
+        // echoing JSON response
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
