9c2f19ee2797cfebe6bbc7912ecbbe1e85a7394f
pricejoshua <joshua.j.price@my.wheaton.edu>
2020-04-30 21:15:32 -0700

    Get user events

--- agenda-buddy/get_user_events.php
+++ agenda-buddy/get_user_events.php
@@ -2,7 +2,7 @@
  
 /*
  * Following code will get single event details
- * A event is identified by user id (uid)
+ * A event is identified by event id (pid)
  */
  
 // array for JSON response
@@ -13,40 +13,48 @@ $con = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE) or die(mysqli_er
 
  
 // check for post data
-if (isset($_POST["eid"])) {
-    $eid = $_POST['eid'];
+if (isset($_POST["user_email"])) {
+    $type = $_POST['type'];
     
-    $result = mysqli_query($con, "SELECT * FROM events WHERE uid = $uid");
+    $result = mysqli_query($con, "SELECT * FROM events WHERE user_email = '$user_email'");
 
- 
+
     if (!empty($result)) {
         // check for empty result
         if (mysqli_num_rows($result) > 0) {
- 
-            $result = mysqli_fetch_array($result);
-                        
-            $event = array();
-            $event["eid"] = $result["eid"];
-            $event["title"] = $result["title"];
-            $event["location"] = $result["location"];
-            $event["start_time"] = $result["start_time"];
-            $event["end_time"] = $result["end_time"];
 
-            // success
+            $response["events"] = array();
+
+            while ($row = mysqli_fetch_array($result)) {
+                $event = array();
+                $event["title"] = $row["title"];
+                $event["start_time"] = $row["start_time"];
+                $event["end_time"] = $row["end_time"];
+                $event["year"] = $row["year"];
+                $event["month"] = $row["month"];
+                $event["day"] = $row["day"];
+                $event["notes"] = $row["notes"];
+                $event["priority"] = $row["priority"];
+                $event["weather"] = $row["weather"];
+                $event["outside"] = $row["outside"];
+                $event["notify"] = $row["notify"];
+                $event["user_email"] = $row["user_email"];
+
+                
+                
+                $event["type"] = $row["type"];
+                array_push($response["events"], $event);
+            }
+
             $response["success"] = 1;
- 
-            // user node
-            $response["event"] = array();
- 
-            array_push($response["event"], $event);
- 
+
             // echoing JSON response
             echo json_encode($response);
         } else {
             // no event found
             $response["success"] = 0;
             $response["message"] = "No event found";
- 
+
             // echo no users JSON
             echo json_encode($response);
         }
@@ -55,17 +63,10 @@ if (isset($_POST["eid"])) {
         $response["success"] = 0;
         $response["message"] = "No event found";
         $response["result"] = $result;
- 
+
         // echo no users JSON
         echo json_encode($response);
     }
-} else {
-    // required field is missing
-    $response["success"] = 0;
-    $response["message"] = "Required field(s) is missing";
- 
-    // echoing JSON response
-    echo json_encode($response);
 }
 ?>
 
